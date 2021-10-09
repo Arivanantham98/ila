e134ad6addc5bebf514d3e766063cefe5bebf3ad
Arivanantham98 <arivanantham1003@gmail.com>
2021-10-09 17:33:44 +0530

    first

---
+++ resources/views/paralegal/index.blade.php
@@ -0,0 +1,333 @@
+@extends('layouts.master')
+@section('title', 'Paralegal Services ')
+@section('content')
+    <div class="page">
+        <!-- page-title -->
+        <div class="ttm-page-title-row">
+            <div class="ttm-page-title-row-inner">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="page-title-heading">
+                                <h2 class="title">Paralegal Services</h2>
+                            </div>
+                            <div class="breadcrumb-wrapper">
+                                <span>
+                                    <a title="Homepage" href="{{ url('/') }}">Home</a>
+                                </span>
+                                <span>About Us</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- page-title end-->
+        <section class="ttm-row ttm-grid-view pb-60 res-991-pb-30 clearfix">
+            <div class="container">
+                <div class="main">
+
+                    <!--cards -->
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://cdn.pixabay.com/photo/2015/11/07/11/41/lake-1031405_1280.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   <!--cards -->
+                   
+                   
+                   <div class="card">
+                   
+                   <div class="image">
+                      <img src="https://cdn.pixabay.com/photo/2015/11/07/11/41/lake-1031405_1280.jpg">
+                   </div>
+                   <div class="title">
+                   
+                   </div>
+                   <div class="des">
+                    <p>You can Add Desccription Here...</p>
+                   
+                   </div>
+                   </div>
+                   </div>
+                {{-- <div class="row">
+                   
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-team ttm-team-box-view2">
+                            <div class="ttm-team-box-view-overlay">
+                                <div class="featured-thumbnail">
+                                    <img class="img-fluid" src="images/team-member/team-img01.jpg" alt="image"> 
+                                </div>
+                                <div class="ttm-team-box-view-overlay-inner">
+                                    <div class="featured-iconbox ttm-media-link">
+                                        <a href="team-details.html"><i class="ti ti-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="featured-content featured-content-team text-center">
+                                <p class="category">Family Lawyer</p>
+                                <div class="featured-title">
+                                    <h5><a href="team-details.html">Alex Edward</a></h5>
+                                </div>
+                                <div class="ttm-social-links-wrapper">
+                                    <ul class="social-icons list-inline">
+                                        <li class="social-facebook"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Facebook"><i class="ti ti-facebook"></i></a></li>
+                                        <li class="social-twitter"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Twitter"><i class="ti ti-twitter-alt"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Instagram"><i class="ti ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-team ttm-team-box-view2">
+                            <div class="ttm-team-box-view-overlay">
+                                <div class="featured-thumbnail">
+                                    <img class="img-fluid" src="images/team-member/team-img02.jpg" alt="image">
+                                </div>
+                                <div class="ttm-team-box-view-overlay-inner">
+                                    <div class="featured-iconbox ttm-media-link">
+                                        <a href="team-details.html"><i class="ti ti-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="featured-content featured-content-team text-center">
+                                <p class="category">Civil Lawyer</p>
+                                <div class="featured-title">
+                                    <h5><a href="team-details.html">Harly Rayan</a></h5>
+                                </div>
+                                <div class="ttm-social-links-wrapper">
+                                    <ul class="social-icons list-inline">
+                                        <li class="social-facebook"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Facebook"><i class="ti ti-facebook"></i></a></li>
+                                        <li class="social-twitter"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Twitter"><i class="ti ti-twitter-alt"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Instagram"><i class="ti ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-team ttm-team-box-view2">
+                            <div class="ttm-team-box-view-overlay">
+                                <div class="featured-thumbnail">
+                                    <img class="img-fluid" src="images/team-member/team-img03.jpg" alt="image"> 
+                                </div>
+                                <div class="ttm-team-box-view-overlay-inner">
+                                    <div class="featured-iconbox ttm-media-link">
+                                        <a href="team-details.html"><i class="ti ti-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="featured-content featured-content-team text-center">
+                                <p class="category">Criminal Lawyer</p>
+                                <div class="featured-title">
+                                    <h5><a href="team-details.html">Andrew Bon</a></h5>
+                                </div>
+                                <div class="ttm-social-links-wrapper">
+                                    <ul class="social-icons list-inline">
+                                        <li class="social-facebook"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Facebook"><i class="ti ti-facebook"></i></a></li>
+                                        <li class="social-twitter"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Twitter"><i class="ti ti-twitter-alt"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Instagram"><i class="ti ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-team ttm-team-box-view2">
+                            <div class="ttm-team-box-view-overlay">
+                                <div class="featured-thumbnail">
+                                    <img class="img-fluid" src="images/team-member/team-img04.jpg" alt="image"> 
+                                </div>
+                                <div class="ttm-team-box-view-overlay-inner">
+                                    <div class="featured-iconbox ttm-media-link">
+                                        <a href="team-details.html"><i class="ti ti-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="featured-content featured-content-team text-center">
+                                <p class="category">Financial Lawyer</p>
+                                <div class="featured-title">
+                                    <h5><a href="team-details.html">John Martin</a></h5>
+                                </div>
+                                <div class="ttm-social-links-wrapper">
+                                    <ul class="social-icons list-inline">
+                                        <li class="social-facebook"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Facebook"><i class="ti ti-facebook"></i></a></li>
+                                        <li class="social-twitter"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Twitter"><i class="ti ti-twitter-alt"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Instagram"><i class="ti ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-team ttm-team-box-view2">
+                            <div class="ttm-team-box-view-overlay">
+                                <div class="featured-thumbnail">
+                                    <img class="img-fluid" src="images/team-member/team-img05.jpg" alt="image"> 
+                                </div>
+                                <div class="ttm-team-box-view-overlay-inner">
+                                    <div class="featured-iconbox ttm-media-link">
+                                        <a href="team-details.html"><i class="ti ti-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="featured-content featured-content-team text-center">
+                                <p class="category">Bankruptcy Lawyer</p>
+                                <div class="featured-title">
+                                    <h5><a href="team-details.html">Thomas Charlie</a></h5>
+                                </div>
+                                <div class="ttm-social-links-wrapper">
+                                    <ul class="social-icons list-inline">
+                                        <li class="social-facebook"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Facebook"><i class="ti ti-facebook"></i></a></li>
+                                        <li class="social-twitter"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Twitter"><i class="ti ti-twitter-alt"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Instagram"><i class="ti ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <!-- featured-imagebox-team -->
+                        <div class="featured-imagebox featured-imagebox-team ttm-team-box-view2">
+                            <div class="ttm-team-box-view-overlay">
+                                <div class="featured-thumbnail">
+                                    <img class="img-fluid" src="images/team-member/team-img06.jpg" alt="image"> 
+                                </div>
+                                <div class="ttm-team-box-view-overlay-inner">
+                                    <div class="featured-iconbox ttm-media-link">
+                                        <a href="team-details.html"><i class="ti ti-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="featured-content featured-content-team text-center">
+                                <p class="category">Immigration Lawyer</p>
+                                <div class="featured-title">
+                                    <h5><a href="team-details.html">Jack Callum</a></h5>
+                                </div>
+                                <div class="ttm-social-links-wrapper">
+                                    <ul class="social-icons list-inline">
+                                        <li class="social-facebook"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Facebook"><i class="ti ti-facebook"></i></a></li>
+                                        <li class="social-twitter"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Twitter"><i class="ti ti-twitter-alt"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="" data-tooltip="Instagram"><i class="ti ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- featured-imagebox-team end-->
+                    </div>
+                </div> --}}
+            </div>
+        </section>
+    </div>
+    <!--site-main end-->
+
+
+@endsection
